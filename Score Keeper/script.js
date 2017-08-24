943bb4d4adbb61000303c71eba30caf5827ea46d
Aleksandr Guzhyk <aleksandr.guzhyk@gmail.com>
2017-08-24 11:33:58 +0300

    finished changes

--- Score Keeper/script.js
+++ Score Keeper/script.js
@@ -3,16 +3,27 @@ var p2Button = document.getElementById("p2");
 var resetButton = document.getElementById("reset");
 var p1Display = document.getElementById("p1Display");
 var p2Display = document.getElementById("p2Display");
+var numInput = document.querySelector("input[type='number']");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score = 0;
 var p2Score = 0;
 var gameOver = false;
-var vinningScore = 5;
+var winningScore = 5;
 
+function reset(){
+  p1Score = 0;
+  p2Score = 0;
+  p1Display.textContent = 0;
+  p2Display.textContent = 0;
+  p1Display.classList.remove("winner");
+  p2Display.classList.remove("winner");
+  gameOver = false;
+};
 
 p1Button.addEventListener("click", function () {
   if (!gameOver) {
     p1Score++;
-    if (p1Score === vinningScore) {
+    if (p1Score === winningScore) {
       p1Display.classList.add("winner")
       gameOver = true;
     }
@@ -23,15 +34,20 @@ p1Button.addEventListener("click", function () {
 p2Button.addEventListener("click", function () {
   if (!gameOver) {
     p2Score++;
-    if (p2Score === vinningScore) {
-      p2Display.classList.add("winner")
+    if (p2Score === winningScore) {
+      p2Display.classList.add("winner");
       gameOver = true;
     }
     p2Display.textContent = p2Score;
   }
 });
 
-resetbutton.addEventListener("click", function () {
-  p1Score = 0;
-  p2Score = 0;
+resetButton.addEventListener("click", function () {
+  reset();
+});
+
+numInput.addEventListener("change", function(){
+  winningScoreDisplay.textContent = this.value;
+  winningScore = Number(this.value);
+  reset();
 });
